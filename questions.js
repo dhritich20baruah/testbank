a9972b34a8be9a1dd1278ef804169c49836f0e75
Delta <dhriti.baruah20@gmail.com>
2022-06-09 20:21:16 +0530

    scorecard

---
+++ questions.js
@@ -0,0 +1,66 @@
+export const questions = [
+    {
+        "Number": 1,
+        "question": "/images/physics/P01.JPG",
+        "answer": 'C',
+        "section": 1
+    },
+    {
+        "Number": 2,
+        "question": "/images/physics/P02.JPG",
+        "answer": 'C',
+        "section": 1
+    },
+    {
+        "Number": 3,
+        "question": "/images/physics/P03.JPG",
+        "answer": 'B',
+        "section": 1
+    },
+    {
+        "Number": 4,
+        "question": "/images/physics/P04.JPG",
+        "answer": 'D',
+        "section": 1
+    },
+    {
+        "Number": 5,
+        "question": "/images/physics/P05.JPG",
+        "min": 0.45,
+        "max": 0.55,
+        "section": 2
+    },
+    {
+        "Number": 6,
+        "question": "/images/physics/P06.JPG",
+        "min": 7.3,
+        "max": 7.7,
+        "section": 2,
+    },
+    {
+        "Number": 7,
+        "question": "/images/maths/m01.JPG",
+        "answer": 'B',
+        "section": 1
+    },
+    {
+        "Number": 8,
+        "question": "/images/maths/m02.JPG",
+        "answer": 'A',
+        "section": 1
+    },
+    {
+        "Number": 9,
+        "question": "/images/maths/m03.JPG",
+        "answer": 'A',
+        "section": 1
+    },
+    {
+        "Number": 10,
+        "question": "/images/maths/m04.JPG",
+        "answer": 'C',
+        "section": 1
+    }
+]
+
+
